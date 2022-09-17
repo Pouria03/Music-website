23a7e5eef5b7cff81775ac4550adc9764f5bae16
pouria03 <pouriash10@hotmail.com>
2022-09-17 11:03:09 +0430

    add corrections to song app

--- song/models.py
+++ song/models.py
@@ -1,20 +1,48 @@
+from tabnanny import verbose
 from django.db import models
-
+from django.urls import reverse
+from django.utils.text import slugify
+# ====================================================
 class Category(models.Model):
     title = models.CharField(max_length=150)
     slug = models.SlugField()
-
+    
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('song:category',kwargs={'slug':self.slug})  
+
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args,**kwargs)
+
+
+    
+    class Meta:
+        verbose_name_plural = 'categories'
+
+# ====================================================
 
 class Artist(models.Model):
     name = models.CharField(max_length=200)
     slug= models.SlugField()
-
+    
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('song:artist',kwargs={'slug':self.slug})
+
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args,**kwargs)
+
+
+
+# ====================================================
 
 class Song(models.Model):
     title = models.CharField(max_length=150)
@@ -23,7 +51,21 @@ class Song(models.Model):
     image = models.ImageField(upload_to='music/%Y/%m/%d/')
     file = models.FileField(upload_to='music/%Y/%m/%d/')
     lyrics = models.TextField()
-    artist = models.ForeignKey(Artist,on_delete=models.SET_NULL,related_name='musics')
+    artist = models.ForeignKey(Artist,on_delete=models.DO_NOTHING,related_name='musics')
+    created_time = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.title
+
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        return reverse('song:detail',kwargs={'pk':self.pk,'slug':self.slug})
+
+    class Meta:
+        ordering = ['-created_time']
+    
+    
--- song/views.py
+++ song/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.views import View
+from .models import Song
 # Create your views here.
+
+# class MusicListView(View):
+#     template_name = 'song/songs.html'
+#     def get(self,request):
+#         songs = Song.objects.all()
+#         return render(request,self.template_name,{'songs':songs})
+
+
